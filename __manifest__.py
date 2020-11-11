92ac08f4bb901a15984a536dbf8961220e103683
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-11-11 16:53:51 +0200

    Added new data files

--- __manifest__.py
+++ __manifest__.py
@@ -4,5 +4,12 @@
 'author': 'Ahmed F.',
 'depends': ['library_app', 'mail'],
 'application': False,
-'data': [ 'security/library_security.xml'],
+'data': [
+'views/book_view.xml',
+'security/library_security.xml',
+'security/ir.model.access.csv',
+'views/member_view.xml',
+'views/library_menu.xml',
+'views/book_list_template.xml',
+],
 }
