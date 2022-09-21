5b26f1582d30bc0414aecd1b1810e6b28a878510
Ahmed Farag <ahmedfarag.a12@gmail.com>
2022-09-21 05:43:58 +0200

    Remove indentation

--- models/library_book.py
+++ models/library_book.py
@@ -9,10 +9,10 @@ class Book(models.Model):
     def _check_isbn(self):
         self.ensure_one()
         digits = [int(x) for x in self.isbn if x.isdigit()]
-            if len(digits) == 10:
-                ponderators = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-                total = sum(a * b for a, b in zip(digits[:9], ponderators))
-                check = total % 11
-                return digits[-1] == check
-            else:
-                return super()._check_isbn()
+        if len(digits) == 10:
+            ponderators = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+            total = sum(a * b for a, b in zip(digits[:9], ponderators))
+            check = total % 11
+            return digits[-1] == check
+        else:
+            return super()._check_isbn()
